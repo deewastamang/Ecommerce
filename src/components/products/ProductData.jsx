7d1ecf3f0c61c44e9609bf0b1f6c62a1614026f4
Deewas Tamang <dtamang1324@gmail.com>
2024-06-12 20:42:57 +0545

    .

--- src/components/products/ProductData.jsx
+++ src/components/products/ProductData.jsx
@@ -15,13 +15,6 @@ import { toast } from "sonner";
 const ProductData = ({ product }) => {
   const dispatch = useDispatch();
 
-
-  const starArray = Array.from({ length: product.rating }, (_, index) => (
-    <span key={index} className="text-yellow-400">
-      <IoIosStar />
-    </span>
-  ));
-
   const [ref, inView] = useInView({
     triggerOnce: true,
     threshold: 0.2,
@@ -48,6 +41,12 @@ const ProductData = ({ product }) => {
     }
   }, [inView]);
 
+  const starArray = Array.from({ length: product.rating }, (_, index) => (
+    <span key={index} className="text-yellow-400">
+      <IoIosStar />
+    </span>
+  ));
+
   return (
     <motion.div
       ref={ref}
@@ -60,7 +59,10 @@ const ProductData = ({ product }) => {
         <div className="relative h-96 group overflow-hidden">
           <Image
             className="object-cover group-hover:scale-105 duration-500"
-            src={product?.image[0] || "https://img.freepik.com/free-vector/shopping-sale-carry-bag-emblem_98292-4007.jpg?t=st=1717402526~exp=1717406126~hmac=c52c9a2c0ec62b9ce7395ed1e6a16e31eed2f4b27f9798648069cf096b7d7949&w=1380"}
+            src={
+              product?.image[0] ||
+              "https://img.freepik.com/free-vector/shopping-sale-carry-bag-emblem_98292-4007.jpg?t=st=1717402526~exp=1717406126~hmac=c52c9a2c0ec62b9ce7395ed1e6a16e31eed2f4b27f9798648069cf096b7d7949&w=1380"
+            }
             alt={`${product.image[0]} image`}
             fill
           />
@@ -73,7 +75,9 @@ const ProductData = ({ product }) => {
         </div>
       </Link>
       <div className="border border-slate-300 border-t-0 px-2 py-4 flex flex-col gap-y-4 bg-white rounded-b-lg ">
-        <Link href={`/products/${product._id}`}><p className="text-md font-medium">{product?.title}</p></Link>
+        <Link href={`/products/${product._id}`}>
+          <p className="text-md font-medium">{product?.title}</p>
+        </Link>
         <div className="flex justify-between items-center">
           <div className="border-[1px] border-orange-600 py-1 px-4 rounded-full text-xs text-muted select-none">
             <p>{calculatePercentage(product.price, product.oldPrice)}% off</p>
@@ -88,7 +92,15 @@ const ProductData = ({ product }) => {
           </div>
         </div>
         <div className="flex justify-between items-center">
-          <button onClick={() => dispatch(addToCart(product)) && toast.success(`${product.title.substring(0,15)} is added to cart.`)} className="whitespace-nowrap flex items-center gap-x-2 bg-orange-600 px-4 py-2 text-sm tracking-wide rounded-full text-slate-100 hover:bg-orange-800 hover:text-white duration-200">
+          <button
+            onClick={() =>
+              dispatch(addToCart(product)) &&
+              toast.success(
+                `${product.title.substring(0, 15)} is added to cart.`
+              )
+            }
+            className="whitespace-nowrap flex items-center gap-x-2 bg-orange-600 px-4 py-2 text-sm tracking-wide rounded-full text-slate-100 hover:bg-orange-800 hover:text-white duration-200"
+          >
             <CartIcon className="text-xl" />
             Add to cart
           </button>
